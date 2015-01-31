1a95b427fb22fcedcc4a55a47bfc6938d7771cc3
DatsivStepan <datsivStepan@gmail.com>
2015-01-31 17:47:13 +0200

    create widget for Latest News

--- views/user/_sidebar.php
+++ views/user/_sidebar.php
@@ -1,6 +1,7 @@
 <?php
 use yii\widgets\ActiveForm;
 use yii\helpers\Html;
+use app\components\LatestNews;
 ?>
 </div>
 <style>
@@ -46,23 +47,6 @@ use yii\helpers\Html;
 <?php if(($this->context->getRoute() == "post/index") || (strpos($this->context->getRoute(),'post') === 0)) : ?>
     
         <div class="col-xs-3">
-            <h3 style="color:#008B66;font-style: italic;">Останні пости</h3>
-            <?php if ($postSidebar) foreach ($postSidebar as $post) : ?>
-                <div class="panel panel-default">
-                    <div style="background-color:#00A87B;" class="panel-heading"><p style="margin-left:10px; font-size:20px;color:#fff;font-style: italic;"><?php echo $post->title."<br>"; ?></p></div>
-                    <div class="panel-body">
-                            <?php if ($post->images) foreach($post->images as $postImage): ?>
-                            <?php //echo $postImage->getImageUrl('small'); ?>
-                                    <img style="width:80px; margin:0px 5px 2px 2px; float:left;box-shadow:0 0 2px #9d9d9d;" src="<?php echo $postImage->getImageUrl('small'); ?>">
-                            <?php    break;
-                             endforeach; ?>
-                        <div style="width:100%;">
-                            <?= mb_substr($post->content, 0, 100, "UTF-8")."..."; ?>
-                        </div>
-                        <button type="submit" class="btn btn-default pull-right"><?php  echo Html::a("Дочитати", array('post/show', 'id'=>$post->id));  ?></button>
-                    </div>
-                </div>
-                <hr>
-            <?php endforeach; ?>
+           <?= LatestNews::widget(['order' => 'ASC', 'limit' => '3']) ?>
         </div>
 <?php endif; ?>
